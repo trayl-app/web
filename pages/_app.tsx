65bc74e5389b863bb3c551579c94ef46b0a243d7
ocintnaf <ocintnaf@gmail.com>
2022-09-18 11:56:13 +0200

    setup eslint and prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import "../styles/globals.css";
-import "@mountain-app/web-components/dist/tailwind.css";
-import type { AppProps } from "next/app";
+import React from 'react';
+import '../styles/globals.css';
+import '@mountain-app/web-components/dist/tailwind.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
