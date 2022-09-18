3ff9c5f30f7f2bc4e7fb7836578933063c625fe0
ocintnaf <ocintnaf@gmail.com>
2022-09-18 11:47:35 +0200

    install @mountain-app/web-components library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { Button, Icon, IconButton } from "@mountain-app/web-components";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -12,14 +13,18 @@ const Home: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
+          Welcome to{" "}
+          <a className="text-brand-primary" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
 
+        <Button variant="secondary-light" sx="rounded-full hover:shadow-xl">
+          ciao
+        </Button>
+
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
             pages/index.tsx
           </code>
@@ -75,12 +80,12 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
